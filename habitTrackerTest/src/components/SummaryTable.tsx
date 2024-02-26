90a3e23fb899bbb7df011279ce48f95593b1890b
Pedro Lucas <pedrolucas122737@gmail.com>
2024-02-26 10:35:57 -0300

    feat: custom horizontal scrolbar

--- habitTrackerTest/src/components/SummaryTable.tsx
+++ habitTrackerTest/src/components/SummaryTable.tsx
@@ -7,6 +7,8 @@ import dayjs from "dayjs";
 import { api } from "../lib/axios";
 // hooks
 import { useEffect, useState } from "react";
+//Styles
+import "../styles/scrollBar.css";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
@@ -32,47 +34,46 @@ const SummaryTable = () => {
   }, []);
 
   return (
-    <div className="w-full flex">
-      {/* Header */}
-      <div className="grid grid-rows-7 grid-flow-row gap-3">
-        {weekDays.map((weekDay, i) => (
-          <div
-            key={i}
-            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
-          >
-            {weekDay}
-          </div>
-        ))}
-      </div>
-
-      {/* Day square */}
-      <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summary &&
-          summary.length > 0 &&
-          summaryDates.map((date) => {
-            const dayInSummary = summary.find((day) => {
-              return dayjs(date).isSame(day.date, "day");
-            });
-
-            return (
-              <HabitDay
-                key={date.toString()}
-                date={date}
-                amount={dayInSummary?.amount}
-                defaultCompleted={dayInSummary?.completed}
-              />
-            );
-          })}
-
-        {amountOfDaysToFill > 0 &&
-          Array.from({ length: amountOfDaysToFill }).map((_, i) => {
-            return (
-              <div
-                key={i}
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
-              />
-            );
-          })}
+    <div className="overflow-x-scroll p-1 custom-scrollbar">
+      <div className="w-full flex">
+        {/* Header */}
+        <div className="grid grid-rows-7 grid-flow-row gap-3">
+          {weekDays.map((weekDay, i) => (
+            <div
+              key={i}
+              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+            >
+              {weekDay}
+            </div>
+          ))}
+        </div>
+        {/* Day square */}
+        <div className="grid grid-rows-7 grid-flow-col gap-3">
+          {summary &&
+            summary.length > 0 &&
+            summaryDates.map((date) => {
+              const dayInSummary = summary.find((day) => {
+                return dayjs(date).isSame(day.date, "day");
+              });
+              return (
+                <HabitDay
+                  key={date.toString()}
+                  date={date}
+                  amount={dayInSummary?.amount}
+                  defaultCompleted={dayInSummary?.completed}
+                />
+              );
+            })}
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+              return (
+                <div
+                  key={i}
+                  className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                />
+              );
+            })}
+        </div>
       </div>
     </div>
   );
--- habitTrackerTest/src/utils/generate-dates-from-year-beginning.ts
+++ habitTrackerTest/src/utils/generate-dates-from-year-beginning.ts
@@ -14,3 +14,59 @@ export function generateDatesFromYearBeginning() {
 
   return dates;
 }
+
+// import dayjs from "dayjs";
+
+// export function generateDatesFromYearBeginning() {
+//   // Defina a data para o primeiro dia de 2023
+//   const firstDayOfTheYear = dayjs("2023-01-01");
+
+//   // Obtenha a data de hoje
+//   const today = dayjs();
+
+//   const dates = [];
+//   let compareDate = firstDayOfTheYear;
+
+//   while (compareDate.isBefore(today)) {
+//     dates.push(compareDate.toDate());
+//     compareDate = compareDate.add(1, "day");
+//   }
+
+//   return dates;
+// }
+
+// import dayjs from "dayjs";
+
+// export function generateDatesFromYearBeginning(
+//   startYear: number,
+//   endYear: number
+// ) {
+//   const today = dayjs();
+//   const currentYear = today.year();
+
+//   // Verifique se o ano inicial está dentro do intervalo permitido
+//   startYear = Math.max(startYear, currentYear - 1);
+
+//   const dates: Date[] = [];
+
+//   // Para cada ano a partir do ano inicial até o ano final
+//   for (let year = startYear; year <= endYear; year++) {
+//     // Defina a data para o primeiro dia do ano
+//     const firstDayOfYear = dayjs(`${year}-01-01`);
+
+//     // Defina a data para o último dia do ano
+//     const lastDayOfYear = dayjs(`${year}-12-31`);
+
+//     // Adicione datas até o final do ano
+//     let currentDate = firstDayOfYear;
+//     while (
+//       currentDate.isBefore(lastDayOfYear) ||
+//       currentDate.isSame(lastDayOfYear, "day")
+//     ) {
+//       dates.push(currentDate.toDate());
+//       currentDate = currentDate.add(1, "day");
+//     }
+//   }
+
+//   return dates;
+// }
